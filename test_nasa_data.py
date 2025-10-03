9906d329b35718b5c31fe729d74bfaedf964faa6
Swadhin18 <144611471+Swadhin18@users.noreply.github.com>
2025-10-03 14:28:10 +0600

    Update test_nasa_data.py

--- test_nasa_data.py
+++ test_nasa_data.py
@@ -1,12 +1,10 @@
 from training.data_downloader import NASADataDownloader
-from utils.config import STUDY_REGIONS
 
 def test_nasa_integration():
     print("🧪 Testing NASA Data Integration...")
     
     downloader = NASADataDownloader()
     
-    # Test data download
     data = downloader.get_complete_satellite_data(
         date="2024-03-15", 
         region_name="gulf_stream"
